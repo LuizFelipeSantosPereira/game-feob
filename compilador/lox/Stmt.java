c293a02404a18ca8594c593562f86244f5719794
Luiz Felipe dos Santos Pereira <66455085+LuizFelipeSantosPereira@users.noreply.github.com>
2025-10-25 22:27:52 -0300

    Add files via upload

---
+++ compilador/lox/Stmt.java
@@ -0,0 +1,88 @@
+package lox;
+
+import java.util.List;
+
+abstract class Stmt {
+    interface Visitor<R> {
+        R visitExpressionStmt(Expression stmt);
+        R visitPrintStmt(Print stmt);
+        R visitVarStmt(Var stmt);
+        R visitBlockStmt(Block stmt);
+        R visitIfStmt(If stmt);
+        R visitWhileStmt(While stmt);
+        R visitFunctionStmt(Function stmt);
+        R visitReturnStmt(Return stmt);
+        R visitClassStmt(Class stmt);
+    }
+
+    static class Expression extends Stmt {
+        final Expr expression;
+        Expression(Expr expression) { this.expression = expression; }
+        <R> R accept(Visitor<R> visitor) { return visitor.visitExpressionStmt(this); }
+    }
+
+    static class Print extends Stmt {
+        final Expr expression;
+        Print(Expr expression) { this.expression = expression; }
+        <R> R accept(Visitor<R> visitor) { return visitor.visitPrintStmt(this); }
+    }
+
+    static class Var extends Stmt {
+        final Token name;
+        final Expr initializer;
+        Var(Token name, Expr initializer) { this.name = name; this.initializer = initializer; }
+        <R> R accept(Visitor<R> visitor) { return visitor.visitVarStmt(this); }
+    }
+
+    static class Block extends Stmt {
+        final List<Stmt> statements;
+        Block(List<Stmt> statements) { this.statements = statements; }
+        <R> R accept(Visitor<R> visitor) { return visitor.visitBlockStmt(this); }
+    }
+
+    static class If extends Stmt {
+        final Expr condition;
+        final Stmt thenBranch;
+        final Stmt elseBranch;
+        If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
+            this.condition = condition; this.thenBranch = thenBranch; this.elseBranch = elseBranch;
+        }
+        <R> R accept(Visitor<R> visitor) { return visitor.visitIfStmt(this); }
+    }
+
+    static class While extends Stmt {
+        final Expr condition;
+        final Stmt body;
+        While(Expr condition, Stmt body) { this.condition = condition; this.body = body; }
+        <R> R accept(Visitor<R> visitor) { return visitor.visitWhileStmt(this); }
+    }
+
+    static class Function extends Stmt {
+        final Token name;
+        final java.util.List<Token> params;
+        final java.util.List<Stmt> body;
+        Function(Token name, java.util.List<Token> params, java.util.List<Stmt> body) {
+            this.name = name; this.params = params; this.body = body;
+        }
+        <R> R accept(Visitor<R> visitor) { return visitor.visitFunctionStmt(this); }
+    }
+
+    static class Return extends Stmt {
+        final Token keyword;
+        final Expr value;
+        Return(Token keyword, Expr value) { this.keyword = keyword; this.value = value; }
+        <R> R accept(Visitor<R> visitor) { return visitor.visitReturnStmt(this); }
+    }
+
+    static class Class extends Stmt {
+        final Token name;
+        final Expr.Variable superclass;
+        final java.util.List<Stmt.Function> methods;
+        Class(Token name, Expr.Variable superclass, java.util.List<Stmt.Function> methods) {
+            this.name = name; this.superclass = superclass; this.methods = methods;
+        }
+        <R> R accept(Visitor<R> visitor) { return visitor.visitClassStmt(this); }
+    }
+
+    abstract <R> R accept(Visitor<R> visitor);
+}
