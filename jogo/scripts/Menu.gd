79c2beb11be9169b54537c9c8ea7d982228fe705
Bruno Henrique <bruno.henrique@sou.unifeov.edu.br>
2025-10-28 22:04:31 -0300

    Transição de cena Menu/Jogo

--- jogo/scripts/Menu.gd
+++ jogo/scripts/Menu.gd
@@ -45,3 +45,11 @@ func _activate_option():
 			print("Options")
 		2:
 			get_tree().quit()
+
+
+func _on_button_pressed() -> void:
+	$CanvasLayer/ColorRect/AnimationPlayer.play("transição")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	get_tree().change_scene_to_file("res://cenas/jogo.tscn")
