056edaad46c28bb17b4f6e981fd6192e45de6577
Bruno Henrique <bruno.henrique@sou.unifeob.edu.br>
2025-11-07 23:49:09 -0300

    Revert "Merge branch 'producao' of https://github.com/LuizFelipeSantosPereira/game-feob into producao"
    This reverts commit cdbe375646722caa1e88d3a6d56aabe210159e4c, reversing
    changes made to 2ae72daa3489ba950a9495c9ef785d7ee2716d22.

---
+++ README.md
@@ -0,0 +1,8 @@
+# game-feob
+
+
+## Responsáveis
+
+- Componentes de Interface: Kamily 
+- Compilador: Luiz
+- Jogo: Bruno, Marcos e Virgilio
--- jogo/scripts/vilao.gd
+++ jogo/scripts/vilao.gd
@@ -1,3 +1,4 @@
-# Este script não é mais usado diretamente
-# O vilão usa o script inline no vilao.tscn
-# Este arquivo é mantido para referência
+extends AnimatedSprite2D
+
+func _ready():
+	play("parado")
